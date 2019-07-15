90e96dbf65f72897ee198d64fb7c50118844d4b5
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-15 17:38:18 -0400

    harry potter football

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const [awayScore, setAwayScore] = useState(32);
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Griffindor</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -23,7 +23,7 @@ const [awayScore, setAwayScore] = useState(32);
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Slytherin</h2>
             <div className="away__score">{ awayScore }</div>
           </div>
         </div>
