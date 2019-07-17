0c753c273487476db522c202d231f7f233286ad4
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-17 08:00:32 -0400

    mvp done in 8 minues

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(0);
-const [awayScore, setAwayScore] = useState(0);
+const [awayScore, setAwayScore] = useState(0);////
 
   return (
     <div className="container">
@@ -32,10 +32,13 @@ const [awayScore, setAwayScore] = useState(0);
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick={() => setHomeScore(homeScore+ 7) } className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick={() => setHomeScore(homeScore+ 3) } className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => setHomeScore(homeScore+ 1) } className="homeButtons__point">Home point</button>
+
         </div>
         <div className="awayButtons">
           <button onClick={() => setAwayScore(awayScore+ 7) } className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick={() => setAwayScore(awayScore+ 3) } className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => setAwayScore(awayScore+ 1) } className="awayButtons__poit">Away point</button>
         </div>
       </section>
     </div>
